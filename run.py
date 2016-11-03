5b9190376349890882ac7d25bf2d8bab4264af60
gaoshiyu <gaoshiyu@intern.douban.com>
2016-11-03 13:15:00 +0800

    init run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+'''
+最顶层的执行脚本
+分别计算paper, author, venue的PR值，
+并存到当前路径下的文本文件
+'''
+# naming convention:
+# gen_paper_link_mat(map_paper2pid(),
+#                    map_paper2outcite_num(non_self))
